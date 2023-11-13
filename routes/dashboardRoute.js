d68e812cb7bc2c5dd492d84800673d6881ae9b49
phpedro06 <xomaicrocefalia123@gmail.com>
2023-11-13 09:40:24 -0300

    teste

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -5,15 +5,18 @@ const DashboardController = require('../controllers/DashboardController');
 const auth = require('../helpers/auth').checkAuth;
 
 router.get('/', auth, DashboardController.dashHome);
+
 router.get('/registergoal', auth, DashboardController.getRegisterGoal);
 router.get('/registerexpense', auth, DashboardController.getRegisterExpense);
 router.get('/registerincome', auth, DashboardController.getRegisterIncome);
+
 router.get('/goal', auth, DashboardController.getGoal);
 router.get('/editgoal/:id', auth, DashboardController.editGoal);
 
 router.post('/registergoal', auth, DashboardController.postRegisterGoal);
 router.post('/registerexpense', auth, DashboardController.postRegisterExpense);
 router.post('/registerincome', auth, DashboardController.postRegisterIncome);
+
 router.post('/editgoal/:id', auth, DashboardController.postEditGoal);
 router.post('/deletegoal/:id', auth, DashboardController.deleteGoal);
 
