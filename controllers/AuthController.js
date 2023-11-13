9a6b549f9fb92e19454b6a6ed2d85248d441c1a8
phpedro06 <xomaicrocefalia123@gmail.com>
2023-11-13 12:07:52 -0300

    edit user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -34,7 +34,7 @@ module.exports = class AuthContoller {
   
         return;
     }
-    if(cpf.length > 14) {
+    if(cpf.length !== 14) {
         req.flash('message', 'Invalid cpf');
         res.render('auth/register');
   
@@ -124,5 +124,103 @@ module.exports = class AuthContoller {
   static logout(req, res) {
     req.session.destroy();
     res.redirect('/auth/login');
+  }
+
+  static async getAccount(req, res) {
+    const userId = req.session.userid;
+
+    const user = await User.findOne({ raw: true, where: { id: userId } });
+
+    const cpf = `${user.cpf[0]}${user.cpf[1]}${user.cpf[2]}.***.***-${user.cpf[12]}${user.cpf[13]}`
+    
+    res.render('auth/account', { user, cpf });
+  }
+
+  static async editAccount(req, res) {
+    const userId = req.session.userid;
+
+    const user = await User.findOne({ raw: true, where: { id: userId } });
+
+    res.render('auth/editaccount', { user });
+  }
+
+  static async postEditAccount(req, res) {
+    const userId = req.session.userid;
+
+    const { name, email, cpf, password, confirmPassword } = req.body;
+
+    if(!name || !email || !cpf) {
+      req.flash('message', 'Invalid credentials');
+      res.redirect('/auth/editaccount');
+      return;
+    }
+
+    let passwordChanged = !(password === undefined || password === null || password === '');
+
+    if(passwordChanged && password !== confirmPassword) {
+      req.flash('message', 'Password does not match!');
+      res.redirect('/auth/editaccount');
+      return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailRegex.test(email)) {
+      req.flash('message', 'Invalid email');
+      res.render('auth/register');
+
+      return;
+    }
+
+    const EmailExist = await User.findOne({ where: { email: email } });
+
+    if(EmailExist && EmailExist.id !== userId) {
+      req.flash('message', 'Email alredy registered');
+      res.render('auth/register');
+
+      return;
+    }
+
+    if(cpf.length !== 14) {
+      req.flash('message', 'Invalid cpf');
+      res.render('auth/register');
+
+      return;
+    }
+
+    const CpfExist = await User.findOne({ where: { cpf: cpf } });
+
+    if(CpfExist && CpfExist.id !== userId) {
+      req.flash('message', 'Cpf alredy registered');
+      res.render('auth/register');
+
+      return;
+    }
+
+    let userUpdate;
+    if(passwordChanged) {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      userUpdate = {
+        name,
+        email,
+        cpf,
+        password: hashedPassword,
+      }
+    } else {
+      userUpdate = {
+        name,
+        email,
+        cpf,
+      }
+    }
+
+    await User.update(userUpdate, { where: { id: userId } })
+      .then(() => {
+        req.session.save(() => {
+          res.redirect(`/auth/account`);
+        });
+      }).catch((err) => console.log(`User update error: ${err}`));
   }
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
+const auth = require('../helpers/auth').checkAuth;
+
 router.get('/register', AuthController.getRegister);
 router.get('/login', AuthController.getLogin);
 router.get('/logout', AuthController.logout);
 
+router.get('/account', auth, AuthController.getAccount);
+router.get('/editaccount', auth, AuthController.editAccount);
+
 router.post('/register', AuthController.postRegister);
 router.post('/login', AuthController.postLogin);
+
+router.post('/editaccount', auth, AuthController.postEditAccount);
 
 module.exports = router;
